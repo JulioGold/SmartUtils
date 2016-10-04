2b65b5cffcf31cd885d3e424889d21bc6e74d671
JulioGold <julio.gold@gmail.com>
2016-10-04 12:46:51 -0300

    Project created.

---
+++ src/SmartUtils/IntHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace SmartUtils
+{
+    public static class IntHelper
+    {
+        public static string NTimes(this int number, int times)
+        {
+            return String.Join(String.Empty, Enumerable.Repeat(number, times));
+        }
+    }
+}
