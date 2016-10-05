d486f9e993034b020acb8a188207e3c14ce7f5c9
Julio Cesar Goldschmidt <julio.gold@gmail.com>
2016-10-05 19:34:18 -0300

    Doc and improvements

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# SmartUtils
-Collection of utils things to use with .NET
+# SmartUtils  
+Collection of utils things to use with .NET  
+  
+  
+Methods
+  
+-------  
+  
+- [IntHelper.NTimes](#IntHelper.NTimes)  
+  
+---  
+### IntHelper.NTimes  
+
+Repeat the number (System.Int32) for n times.   
+  
+```
+Console.WriteLine(1.NTimes(3));
+```  
+Result will be:
+```
+111
+```
+---
+
+### News  
+    
+- 0.0.1 Created the project, added *IntHelper.NTimes* helper method.  
+  
+Danke  
+  
---
+++ src/Run/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartUtils;
+
+namespace Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region IntHelper.NTimes
+
+            Console.WriteLine("<!------- IntHelper.NTimes -------");
+            Console.WriteLine(1.NTimes(3));
+            Console.WriteLine("------- IntHelper.NTimes ------->");
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
